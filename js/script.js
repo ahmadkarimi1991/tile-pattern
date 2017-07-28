ceb2b9c9d77ba340eb745e5e69b00182a59aba58
Ahmad <ahmadkarimi1991@gmail.com>
2017-07-28 21:46:10 +0430

    Add full keyboard support to input

--- js/script.js
+++ js/script.js
@@ -1,67 +1,64 @@
-var input, memory, input_memory;
-input = '';
-memory = [];
-input_memory = [];
-
 function check_memory(number)
 {
     // Memory management
-    var memory_index;
-    memory_index = memory.indexOf(number);
+    var memory = document.getElementById('password').value.split("");
+    var memory_index; = memory.indexOf(number);
     if (memory_index > -1)
     {
         memory.splice(memory_index, 1);
-    }
-    else
-    {
-        memory.push(number);
-    }
-
-    document.getElementById('password').value = memory.join("");
-
-    // Turn on and off tiles
-    if ( document.querySelector('[data-number="' + number + '"]').hasAttribute('data-active') )
-    {
         document.querySelector('[data-number="' + number + '"]').removeAttribute('data-active');
     }
     else
     {
+        memory.push(number);
         document.querySelector('[data-number="' + number + '"]').setAttribute('data-active', '');
     }
+
+    document.getElementById('password').value = memory.join("");
 }
 
 document.querySelectorAll('.cell').forEach(function(cell, index)
 {
     cell.addEventListener('click', function(e)
     {
-        var number;
-        number = this.getAttribute('data-number');
+        var number = this.getAttribute('data-number');
         check_memory(number);
     });
 });
 
 document.getElementById('password').addEventListener('keyup', function(e)
 {
-    var input_last, input_array;
-    input = this.value.toUpperCase();
-    input_last = input.charAt(input.length - 1);
+    var input_array;
+    var input = this.value.toUpperCase();
     input_array = input.split("");
+    var has_error = false;
+    var duplicte = [];
+    var map = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+
+    document.querySelectorAll('.cell').forEach(function(item){
+    	var number = item.getAttribute('data-number');
+		if (input_array.indexOf(number) == -1)
+		{
+			item.removeAttribute('data-active');
+		}
+    });
+
     input_array.forEach(function(item, index)
     {
-        var i = memory.indexOf(item);
-        if ( i > -1 )
+        if (has_error) return;
+
+        var i = duplicte.indexOf(item);
+        if (i > -1 || map.indexOf(item) == -1)
         {
-            document.getElementById('password').classList.add('error');
-            return false;
+            password.classList.add('error');
+            has_error = true;
+            return;
         }
-        else
-        {
-            if ( document.getElementById('password').classList.contains('error') )
-            {
-                document.getElementById('password').classList.remove('error');
-            }
-            memory.push(item);
-            document.querySelector('[data-number="' + item + '"]').setAttribute('data-active', '');
-        }
+        duplicte.push(item);
+        document.querySelector('[data-number="' + item + '"]').setAttribute('data-active', '');
     });
+    if (!has_error)
+    {
+        password.classList.remove('error');
+    }
 });
